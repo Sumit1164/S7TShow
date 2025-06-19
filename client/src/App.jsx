1023886132f1b067c3843f8d9533ca35840a2e6c
Sumit tripathi <sumittripathi8795@gmail.in>
2025-06-20 02:15:09 +0530

    Refactor routing structure in App component and remove StrictMode from main entry point

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/bookings' element={<MyBookings />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+    </>
   )
 }
 
