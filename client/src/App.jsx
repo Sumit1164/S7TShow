ebe41fdd5ec80f984ab3058a05a199feed69ddbb
Sumit tripathi <sumittripathi8795@gmail.in>
2025-06-20 23:51:48 +0530

    Refactor App component by removing unused routes and organizing imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/bookings' element={<MyBookings />} />
-        <Route path='/favorites' element={<Favorites />} />
+        <Route path ='/' element={<Home/>} />
+
       </Routes>
     </>
   )
