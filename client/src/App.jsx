e8c51f7ab2b58f182caf202782210043ccee283e
Sumit tripathi <sumittripathi8795@gmail.in>
2025-06-21 00:01:41 +0530

    Organize imports in App component and clean up route definitions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MoviesDetails from './pages/MoviesDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path ='/' element={<Home/>} />
-
+        <Route path='/' element={<Home/>} />
+        <Route path='/movies' element={<Movies/>} />
+        <Route path='/movies/:id' element={<MoviesDetails/>} />
+        <Route path='/movies/:id/:date' element={<SeatLayout/>} />
+        <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/favorite' element={<Favorite/>} />
+        
       </Routes>
     </>
   )
